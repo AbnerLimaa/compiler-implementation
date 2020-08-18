6074bf2a894c1a47f2deb85169ea98cfa3236ace
Abner Lima <abner.lima27@outlook.com>
2020-08-18 01:05:46 -0300

    abstract syntax tree

--- MiniJava Compiler/src/main/java/Program.java
+++ MiniJava Compiler/src/main/java/Program.java
@@ -1,5 +1,20 @@
+import parser.MiniJavaParser;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.Reader;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("MiniJava Compiler");
+        try {
+            File input_program = new File(args[0]);
+            input_program.createNewFile();
+            Reader targetReader = new FileReader(input_program);
+            MiniJavaParser.parse(targetReader);
+            targetReader.close();
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
